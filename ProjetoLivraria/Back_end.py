84c475915a56003937f8961489011ba103af2274
Lemica22 <karolycamile19@gmail.com>
2022-02-13 16:24:18 -0300

    No ramo apresentacao adicionar arquivos principais

--- ProjetoLivraria/Back_end.py
+++ ProjetoLivraria/Back_end.py
@@ -1,4 +1,5 @@
 import sqlite3
+<<<<<<< HEAD
 
 #def atualiza_banco():
 
@@ -9,6 +10,59 @@ def cadastrar_funcionario_no_banco(lista_dados_funcionario):
     cursor.executemany("""INSERT INTO cadastro_funcionario(codigo_funcionario, nome_funcionario, senha_funcionario,
                                                             endereco_funcionario, telefone_funcionario)
                                          VALUES (?,?,?,?,?)
+=======
+'''
+data_base_livraria = sqlite3.connect('Bookstore.db')
+cursor = data_base_livraria.cursor()
+
+cursor.execute("""
+CREATE TABLE cadastro_funcionario(
+        codigo_funcionario INTERGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        nome_funcionario TEXT NOT NULL,
+        senha_funcionario TEXT NOT NULL,
+        endereco_funcionario TEXT NOT NULL,
+        telefone_funcionario TEXT
+);
+""")
+
+# terminaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaar
+cursor.execute("""
+CREATE TABLE cadastro_cliente(
+        codigo_cliente INTERGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        data_nascimento_cliente DATE NOT NULL,
+        nome_cliente TEXT NOT NULL,
+        endereco_cliente TEXT NOT NULL,
+        email_cliente TEXT NOT NULL,
+        telefone_cliente TEXT,
+        cpf_cliente TEXT,
+        cnpj_cliente TEXT,
+        razao_scoial TEXT
+);
+""")
+
+# terminaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaar
+cursor.execute("""
+CREATE TABLE cadastro_livros(
+        codigo_cliente INTERGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        data_nascimento_cliente DATE NOT NULL,
+        nome_cliente TEXT NOT NULL,
+        endereco_cliente TEXT NOT NULL,
+        email_cliente TEXT NOT NULL,
+        telefone_cliente TEXT,
+        cpf_cliente TEXT,
+        cnpj TEXT,
+        razao_scoial TEXT
+);
+""")
+
+def atualiza_banco():
+
+def cadastrar_funcionario_no_banco(lista_dados_funcionario):
+    data_base_livraria = sqlite3.connect('Bookstore.db.db')
+    cursor = data_base_livraria.cursor()
+    cursor.executemany("""INSERT INTO cadastro_funcionario (nome, idade, cpf, email, fone, cidade, uf, criado_em)
+                                         VALUES (?,?,?,?,?,?,?,?)
+>>>>>>> 5bedf35... Adicinando arquivos do projeto de livraria
                                        """, lista_dados_funcionario)
     data_base_livraria.commit()
     data_base_livraria.close()
@@ -21,13 +75,21 @@ def excluir_funcionario_no_banco(codigo_cliente):
     cursor.close()
 
 def adicionar_clientes(lista_de_informacoes_do_cliente):
+<<<<<<< HEAD
     data_base_livraria = sqlite3.connect('Bookstore.db')
+=======
+    data_base_livraria = sqlite3.connect('Bookstore.db.db')
+>>>>>>> 5bedf35... Adicinando arquivos do projeto de livraria
     cursor = data_base_livraria.cursor()
     cursor.executemany("""INSERT INTO cadastro_clientes (codigo_cliente, data_nascimento_cliente, nome_cliente,
                                       endereco_cliente, email_cliente, telefone_cliente, cpf_cliente, cnpj_cliente,
                                       razao_scoial)
                                              VALUES (?,?,?,?,?,?,?,?,?)
+<<<<<<< HEAD
                                            """, lista_de_informacoes_do_cliente)
+=======
+                                           """, lista_de_informacoes)
+>>>>>>> 5bedf35... Adicinando arquivos do projeto de livraria
     data_base_livraria.commit()
     data_base_livraria.close()
 
@@ -39,19 +101,37 @@ def excluir_cliente(codigo_cliente):
     cursor.close()
 
 def adicionar_livros(lista_de_informacoes_do_livro):
+<<<<<<< HEAD
     data_base_livraria = sqlite3.connect('Bookstore.db')
     cursor = data_base_livraria.cursor()
     cursor.executemany("""INSERT INTO cadastro_livros_novo (ISBN , codigo_cliente, livro , editora  , autor,  
                                                             ano_publicacao , genero, quantidade , preco)
                                              VALUES (?,?,?,?,?,?,?,?,?)
                                            """, lista_de_informacoes_do_livro)
+=======
+    data_base_livraria = sqlite3.connect('Bookstore.db.db')
+    cursor = data_base_livraria.cursor()
+    cursor.executemany("""INSERT INTO cadastro_clientes (codigo_cliente, data_nascimento_cliente, nome_cliente,
+                                      endereco_cliente, email_cliente, telefone_cliente, cpf_cliente, cnpj_cliente,
+                                      razao_scoial)
+                                             VALUES (?,?,?,?,?,?,?,?,?)
+                                           """, lista_de_informacoes)
+>>>>>>> 5bedf35... Adicinando arquivos do projeto de livraria
     data_base_livraria.commit()
     data_base_livraria.close()
 
 def excluir_livro(isbn):
     data_base_livraria = sqlite3.connect('Bookstore.db')
     cursor = data_base_livraria.cursor()
+<<<<<<< HEAD
     cursor.execute("""DELETE from cadastro_livros_novo where codigo_livro = ?""", (isbn,))
     data_base_livraria.commit()
     cursor.close()
 
+=======
+    cursor.execute("""DELETE from cadastro_cliente where codigo_livro = ?""", (isbn,))
+    data_base_livraria.commit()
+    cursor.close()
+
+'''
+>>>>>>> 5bedf35... Adicinando arquivos do projeto de livraria
--- ProjetoLivraria/Front_end.py
+++ ProjetoLivraria/Front_end.py
@@ -26,22 +26,38 @@ def tela_cadastro_funcionario():
     ]
     janela_tela_cadastro_funcionario = sg.Window("Cadastro de Funcionários", layout_tela_cadastro_funcionario)
     button, cont_tela_cadastro_funcionario = janela_tela_cadastro_funcionario.Read()
+<<<<<<< HEAD
     lista_atributos_funcionarios = [(cont_tela_cadastro_funcionario['codigo'], cont_tela_cadastro_funcionario['nome'],
                                     cont_tela_cadastro_funcionario['senha'], cont_tela_cadastro_funcionario['endereco'],
                                     cont_tela_cadastro_funcionario['telefone'])]
 
     cadastrar_funcionario_no_banco(lista_atributos_funcionarios)
+=======
+    lista_atributos_funcionarios = (cont_tela_cadastro_funcionario['codigo'], cont_tela_cadastro_funcionario['nome'],
+                                    cont_tela_cadastro_funcionario['senha'], cont_tela_cadastro_funcionario['endereco'],
+                                    cont_tela_cadastro_funcionario['telefone'])
+    #cadastrar_funcionario_no_banco(lista_atributos_funcionarios)
+>>>>>>> 5bedf35... Adicinando arquivos do projeto de livraria
     return janela_tela_cadastro_funcionario
 
 def tela_inicial_bookstrore():
     sg.change_look_and_feel('Purple')
     layout_tela_inicial = [
         [sg.Text("Bem vindo à Karolyne's Bookstore")],
+<<<<<<< HEAD
         [sg.Button("Funcionário"), sg.Button("Clientes"), sg.Button("Relatório de estoque"), sg.Button("Livros"), sg.Button("Vendas"), sg.Button("Sair")]
     ]
 
     janela_tela_inicial_bookstore = sg.Window("Bookstore").layout(layout_tela_inicial)
     button, cont_tela_inicial = janela_tela_inicial_bookstore.Read()
+=======
+        [sg.Button("Funcionário"), sg.Button("Clientes"), sg.Button("Relatório de estoque")],
+        [sg.Button("Livros"), sg.Button("Vendas"), sg.Button("Sair")]
+    ]
+
+    janela_tela_inicial_bookstore = sg.Window("Bookstore").layout(layout_tela_inicial)
+    button, cont_tela_inicial = janela_tela_inicial.Read()
+>>>>>>> 5bedf35... Adicinando arquivos do projeto de livraria
     return janela_tela_inicial_bookstore
 
 def Dados_Clientes():
@@ -54,7 +70,10 @@ def Dados_Clientes():
     ]
     janela_Dados_Clientes = sg.Window("Dados dos Clientes").layout(layout_tela_Dados_Cliente)
     button, cont_tela_inicial = janela_Dados_Clientes.Read()
+<<<<<<< HEAD
     return janela_Dados_Clientes
+=======
+>>>>>>> 5bedf35... Adicinando arquivos do projeto de livraria
 
 def Dados_Funcionarios():
     sg.change_look_and_feel('Purple')
@@ -77,7 +96,10 @@ def Dados_Livro():
     ]
     janela_Dados_Livros = sg.Window("Dados dos Produtos").layout(layout_tela_Dados_Livros)
     button, cont_tela_Dados_Livros = janela_Dados_Livros.Read()
+<<<<<<< HEAD
     return janela_Dados_Livros
+=======
+>>>>>>> 5bedf35... Adicinando arquivos do projeto de livraria
 
 def tela_cadastro_cliente():
     sg.change_look_and_feel('Purple')
@@ -95,11 +117,15 @@ def tela_cadastro_cliente():
     ]
     janela_tela_cadastro_cliente = sg.Window("Cadastro dos Clientes").layout(layout_tela_cadastro_cliente)
     button, cont_tela_cadastro = janela_tela_cadastro_cliente.Read()
+<<<<<<< HEAD
     return janela_tela_cadastro_cliente
+=======
+>>>>>>> 5bedf35... Adicinando arquivos do projeto de livraria
 
 def tela_cadastro_livro():
     sg.change_look_and_feel('Purple')
     layout_tela_cadastro_livro = [
+<<<<<<< HEAD
         [sg.Text('ISBN:', size=(10, 0)), sg.Input(size=(10, 0), key='ISBN')],
         [sg.Text('codigo_cliente:', size=(10, 0)), sg.Input(size=(5, 0), key='codigo_cliente')],
         [sg.Text('livro:', size=(10, 0)), sg.Input(size=(15, 0), key='livro')],
@@ -111,10 +137,24 @@ def tela_cadastro_livro():
         [sg.Checkbox("Nacional", key='nacional'), sg.Checkbox("Importados", key='importados')],
         [sg.Text('quantidade:', size=(10, 0)), sg.Input(size=(10, 0), key='quantidade')],
         [sg.Text('preco:', size=(10, 0)), sg.Input(size=(20, 0), key='preco')],
+=======
+        [sg.Text('ISBN:', size=(10, 0)), sg.Input(size=(10, 0), key='isbn')],
+        [sg.Text('Código do Cliente:', size=(10, 0)), sg.Input(size=(5, 0), key='codigo_cliente')],
+        [sg.Text('Livro:', size=(10, 0)), sg.Input(size=(15, 0), key='livro')],
+        [sg.Text('Editora:', size=(10, 0)), sg.Input(size=(20, 0), key='editora')],
+        [sg.Text('Autor:', size=(10, 0)), sg.Input(size=(20, 0), key='autor')],
+        [sg.Text('Ano de publicação do livro:', size=(10, 0)), sg.Input(size=(20, 0), key='ano_publicacao')],
+        [sg.Text('Gênero:', size=(10, 0)), sg.Input(size=(20, 0), key='genero')],
+        [sg.Text("Qual a origem do livro?")],
+        [sg.Checkbox("Nacional", key='nacional'), sg.Checkbox("Importados", key='importados')],
+        [sg.Text('Quantidade:', size=(10, 0)), sg.Input(size=(10, 0), key='qtd_livro')],
+        [sg.Text('Preço:', size=(10, 0)), sg.Input(size=(20, 0), key='preco')],
+>>>>>>> 5bedf35... Adicinando arquivos do projeto de livraria
 
         [sg.Button("Cadastrar o livro"), sg.Button("Voltar")]
 
     ]
+<<<<<<< HEAD
 
     janela_tela_cadastro_livro = sg.Window("Cadastro de Livros").layout(layout_tela_cadastro_livro)
     button, conteudo_da_tela = janela_tela_cadastro_livro.Read()
@@ -124,6 +164,10 @@ def tela_cadastro_livro():
 
     adicionar_livros(lista_atributos_livro)
     return janela_tela_cadastro_livro
+=======
+    janela_tela_cadastro_livro = sg.Window("Cadastro de Livros").layout(layout_tela_cadastro_livro)
+    button, conteudo_da_tela = janela_tela_cadastro_livro.Read()
+>>>>>>> 5bedf35... Adicinando arquivos do projeto de livraria
 
 def tela_exibir_livros():
     layout_tela_exibir_livros =[
@@ -131,5 +175,9 @@ def tela_exibir_livros():
 
     ]
     janela_tela_exibir_livros = sg.Window("Livros").layout(layout_tela_exibir_livros)
+<<<<<<< HEAD
     button, cont_tela_exibir_livros = janela_tela_exibir_livros.Read()
     return janela_tela_exibir_livros
+=======
+    button, cont_tela_exibir_livros = janela_tela_exibir_livros.Read()
+>>>>>>> 5bedf35... Adicinando arquivos do projeto de livraria
